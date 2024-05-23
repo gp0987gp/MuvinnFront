95227efb5aaacbe12e2f9abc4aafd619b1af7f13
gp0987gp <gp0987gp@gmail.com>
2024-05-23 08:03:55 -0300

    ATT

--- App.tsx
+++ App.tsx
@@ -1,18 +1,20 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import React from 'react';
-import CadastroImovel from './src/screens/CadastroImovel';
-import CadastroScreen from './src/screens/CadastroImovel';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import React from "react";
+import Muvinn from "./src/screens/Muvinn";
+import CadastroImovel from "./src/screens/CadastroImovel";
+
 const Stack = createStackNavigator();
 
-function App(): React.ReactElement {
-  return (
-    <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name='CadastroImovel' component={CadastroScreen} options={{headerShown: false}}/>
-        </Stack.Navigator>
-    </NavigationContainer>
-  );
+function App(): React.JSX.Element {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name='Muvinn' component={Muvinn}
+                options={{ headerShown: false }} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
 
 export default App;
